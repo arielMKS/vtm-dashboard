f414170db2c5530da28da26b622e98e81515eb5a
Ariel Cook <arielcook101014@gmail.com>
2019-12-29 17:21:03 -0800

    functional component works. converting to class component

--- client/README2.md
+++ client/README2.md
@@ -1,3 +1,6 @@
 # video tutorial
 
+video #6:
 https://www.youtube.com/watch?v=jQyCEEzgnTM
+video #7:
+https://www.youtube.com/watch?v=Jkj_XP80h1k
--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,28 @@
 import React from "react";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
 
 import Layout from "./components/Layout";
+import Login from "./components/Login";
+import Dashboard from "./page/Dashboard";
+
+// const books = ["one", "two", "three", "four"];
+const books = [
+  { id: 0, title: "one" },
+  { id: 1, title: "two" },
+  { id: 2, title: "three" },
+  { id: 3, title: "four" }
+];
 
 function App() {
-  React.useEffect(() => {
-    console.log("app ue firing");
-  });
   return (
-    <div className="App">
-      App
-      <Layout />
+    <div className="">
+      <Layout books={books}>
+        <Switch>
+          <Route exact path="/" component={Layout}></Route>
+          <Route exact path="/login" component={Login}></Route>
+          <Route exact path="/dashboard" component={Dashboard}></Route>
+        </Switch>
+      </Layout>
     </div>
   );
 }
