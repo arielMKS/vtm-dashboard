c379064ca8339332d34fbb6f8ab48a82bbc6b063
Ariel Cook <arielcook101014@gmail.com>
2019-12-30 07:23:43 -0800

    Deleted unused methods

--- client/src/components/Layout/index.js
+++ client/src/components/Layout/index.js
@@ -1,19 +1,14 @@
 import React from "react";
-import Compose from "compose";
 import { MenuList, MenuItem, AppBar } from "@material-ui/core";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Divider from "@material-ui/core/Divider";
 import Drawer from "@material-ui/core/Drawer";
 import Hidden from "@material-ui/core/Hidden";
 import IconButton from "@material-ui/core/IconButton";
-import InboxIcon from "@material-ui/icons/MoveToInbox";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
 import Menu from "@material-ui/icons/Menu";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import { withStyles, makeStyles, useTheme } from "@material-ui/core/styles";
+import { withStyles } from "@material-ui/core/styles";
 
 import { Link } from "react-router-dom";
 
